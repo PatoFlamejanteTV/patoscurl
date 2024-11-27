3c55dd3d0d97f425e4eb875c17f7a549cdab8139
UltimateQuack <patoflamejantetv@gmail.com>
2024-11-27 11:40:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,131 +26,135 @@ SOFTWARE.
 */
 ////////////////////////////////////////////////////
 const express = require("express");
-const fs = require("fs");     // Import the fs module for file system operations
-const os = require("os");     // Import the os module for system information
-const dns = require("dns");   // Import the dns module for DNS resolution
-//const ping = require('ping'); // Import the ping module for pinging
-//const path = require('path');
+const fs = require("fs"); // Import the fs module for file system operations
+const os = require("os"); // Import the os module for system information
+const dns = require("dns"); // Import the dns module for DNS resolution
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//const CURLDEBUGASCII = process.env.CURLDEBUGASCII || 1234;
-
 // Middleware to check if the request is from curl
 function detectCurl(req, res, next) {
-  const userAgent = req.headers["user-agent"] || "";
-  req.isCurl = userAgent.toLowerCase().includes("curl");
-  next();
+	const userAgent = req.headers["user-agent"] || "";
+	req.isCurl = userAgent.toLowerCase()
+		.includes("curl");
+	next();
 }
 
 app.use(detectCurl);
 
 // Route to handle requests
 app.get("/", (req, res) => {
-  //const startTime = Date.now(); // Record start time for delay calculation
-  const ipAddress = req.socket.remoteAddress; // Get client IP address
-
-  console.log("/ " + req.socket.remoteAddress + ". isCURL? " + req.isCurl);
-  if (req.isCurl) {
-    // Respond with the content of indexcurl.txt for curl requests
-    fs.readFile("indexcurl.txt", "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send("Error reading indexcurl.txt");
-      } else {
-        //const endTime = Date.now(); // Record end time for delay calculation
-        //const delay = endTime - startTime; // Calculate the delay
-
-        // Add server info and debug data to the response
-        const serverInfo = `\n\n\nServer Info:\nIP Address: ${ipAddress}\n`;
-        res.type("text/plain"); // Set content type to plain text
-        res.send(`${serverInfo}\n${data}`);
-      }
-    });
-  } else {
-    // Respond with the content of indexbrowser.html for browser requests
-    fs.readFile("indexbrowser.html", "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send("Error reading indexbrowser.html");
-      } else {
-        res.type("text/html");
-        res.send(data);
-      }
-    });
-  }
+	//const startTime = Date.now(); // Record start time for delay calculation
+	const ipAddress = req.socket.remoteAddress; // Get client IP address
+	
+	console.log("/ " + req.socket.remoteAddress + ". isCURL? " + req.isCurl);
+	if (req.isCurl) {
+		// Respond with the content of indexcurl.txt for curl requests
+		fs.readFile("indexcurl.txt", "utf8", (err, data) => {
+			if (err) {
+				console.error(err);
+				res.status(500)
+					.send("Error reading indexcurl.txt");
+			} else {
+				//const endTime = Date.now(); // Record end time for delay calculation
+				//const delay = endTime - startTime; // Calculate the delay
+				
+				// Add server info and debug data to the response
+				const serverInfo = `\n\n\nServer Info:\nIP Address: ${ipAddress}\n`;
+				res.type("text/plain"); // Set content type to plain text
+				res.send(`${serverInfo}\n${data}`);
+			}
+		});
+	} else {
+		// Respond with the content of indexbrowser.html for browser requests
+		fs.readFile("indexbrowser.html", "utf8", (err, data) => {
+			if (err) {
+				console.error(err);
+				res.status(500)
+					.send("Error reading indexbrowser.html");
+			} else {
+				res.type("text/html");
+				res.send(data);
+			}
+		});
+	}
 });
 
 // Route for subpages
 app.get("/:subpage", (req, res) => {
-  console.log("/${subpage}" + req.socket.remoteAddress + ". isCURL? " + req.isCurl);
-  //console.log(req.socket.remoteAddress);
-  const subpage = req.params.subpage;
-  const filePath = `./${subpage}.txt`;
-
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(404).send("Subpage not found");
-    } else {
-      if (req.isCurl) {
-        res.type("text/plain");
-        res.status(200).send("Subpage found");
-        res.send(data);
-      } else {
-        res.type("text/html");
-        res.status(405).send("Please use CURL next time");
-        res.send(data);
-      }
-    }
-  });
+	console.log("/${subpage}" + req.socket.remoteAddress + ". isCURL? " + req.isCurl);
+	//console.log(req.socket.remoteAddress);
+	const subpage = req.params.subpage;
+	const filePath = `./${subpage}.txt`;
+	
+	fs.readFile(filePath, "utf8", (err, data) => {
+		if (err) {
+			console.error(err);
+			res.status(404)
+				.send("Subpage not found"); // 404 Not Found
+		} else {
+			if (req.isCurl) {
+				res.type("text/plain");
+				res.status(200)
+					.send("Subpage found"); // 200 OK
+				res.send(data);
+			} else {
+				res.type("text/html");
+				res.status(405)
+					.send("Please use CURL next time"); // 405 Method Not Allowed
+				res.send(data);
+			}
+		}
+	});
 });
 
 // Route for the debug subpage
 app.get("/TEST/debug", (req, res) => {
-  console.log("/TEST/debug " + req.socket.remoteAddress + ". isCURL? " + req.isCurl);
-  const ipAddress = req.socket.remoteAddress; // Get client IP address
-  const userAgent = req.headers["user-agent"] || "";
-  const hostname = os.hostname(); // Get the hostname of the server
-  const platform = os.platform(); // Get the operating system
-  const arch = os.arch(); // Get the system architecture
-  const totalMemory = os.totalmem() / (1024 * 1024); // Get total memory in MB
-  const freeMemory = os.freemem() / (1024 * 1024); // Get free memory in MB
-  const cpuCount = os.cpus().length; // Get the number of CPU cores
-  const cpuInfo = os.cpus(); // It should return an JSON with ann CPU cores info
-  // Assuming cpuInfo is an array of objects
-  let formattedCpuInfo = "";
-  cpuInfo.forEach((cpu, index) => {
-    // Format each CPU object
-    formattedCpuInfo += `CPU ${index + 1}:\n`;
-    formattedCpuInfo += `  Model: ${cpu.model}\n`;
-    formattedCpuInfo += `  Speed: ${cpu.speed} GHz\n`; 
-    // Add more properties from your cpu object as needed
-    formattedCpuInfo += "\n";
-  });
-  const uptime = os.uptime(); // Get the server uptime in seconds
-  const userInfo = os.userInfo(); // https://github.com/nodejs/node/blob/v22.x/lib/os.js#L352
+	console.log("/TEST/debug " + req.socket.remoteAddress + ". isCURL? " + req.isCurl);
+	const ipAddress = req.socket.remoteAddress; // Get client IP address
+	const userAgent = req.headers["user-agent"] || "";
+	const hostname = os.hostname(); // Get the hostname of the server
+	const platform = os.platform(); // Get the operating system
+	const arch = os.arch(); // Get the system architecture
+	const totalMemory = os.totalmem() / (1024 * 1024); // Get total memory in MB
+	const freeMemory = os.freemem() / (1024 * 1024); // Get free memory in MB
+	const cpuCount = os.cpus()
+		.length; // Get the number of CPU cores
+	const cpuInfo = os.cpus(); // It should return an JSON with ann CPU cores info
+	// Assuming cpuInfo is an array of objects
+	let formattedCpuInfo = "";
+	cpuInfo.forEach((cpu, index) => {
+		// Format each CPU object
+		formattedCpuInfo += `CPU ${index + 1}:\n`;
+		formattedCpuInfo += `  Model: ${cpu.model}\n`;
+		formattedCpuInfo += `  Speed: ${cpu.speed} GHz\n`;
+		// Add more properties from your cpu object as needed
+		formattedCpuInfo += "\n";
+	});
+	const uptime = os.uptime(); // Get the server uptime in seconds
+	const userInfo = os.userInfo(); // https://github.com/nodejs/node/blob/v22.x/lib/os.js#L352
+	
+	// Get DNS information
+	dns.lookup(hostname, (err, addresses) => {
+		if (err) {
+			console.error(err);
+			addresses = "N/A";
+		}
+		
+		/*// Get lines of code
+		let totalLines = 0;
+		const directory = "."; // Current directory
+		const files = fs.readdirSync(directory); // Get all files
 
-  // Get DNS information
-  dns.lookup(hostname, (err, addresses) => {
-    if (err) {
-      console.error(err);
-      addresses = "N/A";
-    }
-
-    /*// Get lines of code
-    let totalLines = 0;
-    const directory = "."; // Current directory
-    const files = fs.readdirSync(directory); // Get all files
-
-    files.forEach((file) => {
-      const filePath = path.join(directory, file); // Create full path
-      const content = fs.readFileSync(filePath, "utf8");
-      totalLines += content.split("\n").length;
-    });*/
-
-    // Construct the debug information
-    const debugInfo = `
+		files.forEach((file) => {
+		  const filePath = path.join(directory, file); // Create full path
+		  const content = fs.readFileSync(filePath, "utf8");
+		  totalLines += content.split("\n").length;
+		});*/
+		
+		// Construct the debug information
+		const debugInfo = `
 Debug Information
 Client IP Address: ${ipAddress}
 User Agent: ${userAgent}
@@ -165,32 +169,32 @@ DNS Lookup (IP): ${addresses}
 CPU Info (Advanced): ${formattedCpuInfo}
 User Info (Advanced): ${userInfo}
         `;
-
-    //console.log(debugInfo);
-
-    // Send the debug information as HTML
-    res.type("text/html");
-    res.send(debugInfo);
-  });
+		
+		//console.log(debugInfo);
+		
+		// Send the debug information as HTML
+		res.type("text/html");
+		res.send(debugInfo);
+	});
 });
 
 // Route for the server memory/hardware subpage
 app.get("/TEST/mem", (req, res) => {
-  console.log("/TEST/mem " + req.socket.remoteAddress + ". isCURL? " + req.isCurl);
-  const totalMemory = os.totalmem() / (1024 * 1024); // Get total memory in MB
-  const freeMemory = os.freemem() / (1024 * 1024); // Get free memory in MB
-  const cpuCount = os.cpus().length; // Get the number of CPU cores
-  const uptime = os.uptime(); // Get the server uptime in seconds
-
-    // Send the debug information as HTML
-    res.type("text/plain");
-    res.send(`
+	console.log("/TEST/mem " + req.socket.remoteAddress + ". isCURL? " + req.isCurl);
+	const totalMemory = os.totalmem() / (1024 * 1024); // Get total memory in MB
+	const freeMemory = os.freemem() / (1024 * 1024); // Get free memory in MB
+	const cpuCount = os.cpus()
+		.length; // Get the number of CPU cores
+	const uptime = os.uptime(); // Get the server uptime in seconds
+	
+	// Send the debug information as HTML
+	res.type("text/plain");
+	res.send(`
             Total Memory: ${totalMemory} MB
             Free Memory: ${freeMemory} MB
             CPU Cores: ${cpuCount} cores
-            Uptime: ${uptime}`
-            );
-  });
+            Uptime: ${uptime}`);
+});
 
 // Route for the ping subpage
 /*app.get("/TEST/ping", (req, res) => {
@@ -225,5 +229,5 @@ app.get("/TEST/mem", (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+	console.log(`Server is running on http://localhost:${PORT}`);
 });
